572d13b4852a1d286dae67240ab7607b4aaa3025
Hakan Akın <cehakanakin@gmail.com>
2022-03-29 22:48:10 +0300

    Added global type as any.

--- src/index.tsx
+++ src/index.tsx
@@ -3,33 +3,33 @@ import { Dimensions, PixelRatio, Platform } from 'react-native';
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
 // Guideline sizes are based on iPhone X screen size
-global.baseWidth = 375;
-global.baseHeight = 812;
-global.scale = SCREEN_WIDTH / 375;
+(global as any).baseWidth = 375;
+(global as any).baseHeight = 812;
+(global as any).scale = SCREEN_WIDTH / 375;
 
 console.log({
-  baseWidth: global.baseWidth,
-  baseHeight: global.baseHeight,
-  scale: global.scale,
+  baseWidth: (global as any).baseWidth,
+  baseHeight: (global as any).baseHeight,
+  scale: (global as any).scale,
 });
 
 
 export const setBaseSize = (width: number, height: number) => {
-  global.baseWidth = width;
-  global.baseHeight = height;
-  scale = SCREEN_WIDTH / width;
+  (global as any).baseWidth = width;
+  (global as any).baseHeight = height;
+  (global as any).scale = SCREEN_WIDTH / width;
 };
 
 export const convertX = (xValue: number): number => {
-  return (SCREEN_WIDTH / global.baseWidth) * xValue;
+  return (SCREEN_WIDTH / (global as any).baseWidth) * xValue;
 };
 
 export const convertY = (yValue: number): number => {
-  return (SCREEN_HEIGHT / global.baseHeight) * yValue;
+  return (SCREEN_HEIGHT / (global as any).baseHeight) * yValue;
 };
 
 export const scaleFont = (size: number): number => {
-  const newSize: number = size * global.scale;
+  const newSize: number = size * (global as any).scale;
 
   if (Platform.OS === 'ios') {
     return Math.round(PixelRatio.roundToNearestPixel(newSize));
