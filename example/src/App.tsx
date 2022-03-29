05fbfc6f18c2e488a4b2823f2fd3f2eebb2411c5
Hakan Akın <cehakanakin@gmail.com>
2022-03-29 20:32:45 +0300

    Added example.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,31 @@
-import * as React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-responsive-pixels';
+import { convertX, convertY, scaleFont, setBaseSize } from 'react-native-responsive-pixels';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [isBaseSizeReady, setBaseSizeReady] = useState<boolean>(false);
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+  useEffect(() => {
+    if (!isBaseSizeReady) {
+      setBaseSize(375, 812);
+      setBaseSizeReady(true);
+    }
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
+    <View style={{
+      width: convertX(375),
+      height: convertY(812),
+      backgroundColor: '#123455',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <Text style={{ fontSize: scaleFont(22), color: 'white' }}>Hakan Akin</Text>
+      <Text style={{ fontSize: scaleFont(18), marginTop: convertY(15), color: 'white' }}>Senior React Native Developer</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
+
 });
